bde24c5ff12c34c3f9f10f7b8bbb37bf257bf26c
sirikwan <k.sirikwann@gmail.com>
2016-03-29 10:29:54 +0700

    finish
    bind widget

--- app/src/main/java/ice_pbru/khamhor/sirikwan/piano/MainActivity.java
+++ app/src/main/java/ice_pbru/khamhor/sirikwan/piano/MainActivity.java
@@ -1,7 +1,9 @@
 package ice_pbru.khamhor.sirikwan.piano;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import java.security.PrivateKey;
@@ -27,8 +29,23 @@ public class MainActivity extends AppCompatActivity {
         //3.Bine widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
 
     }//Main Method
+
+    private void buttonController() {
+        doButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer doMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.dosong);
+                doMediaPlayer.start();
+            }
+        });
+
+
+    }//buttonController
 
     private void bindWidget() {
         doButton = (Button) findViewById(R.id.button1);
